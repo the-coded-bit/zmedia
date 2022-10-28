b688b8c2b78dd3a822429a4055b3a7d5c5fbe2a7
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-10-28 16:44:05 +0530

    feat: navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,20 @@ import './components.css'
 
 function Navbar() {
   return (
-    <nav className='nav-container'>Navbar</nav>
+    <nav className='nav-container'>
+      <div className='search'>
+        <i class="ri-search-line"></i>
+        <input
+          type='text'
+          placeholder='Search.... '
+          style={{ height: '50%', width: '100%', outline: 'none', border: 'none', fontFamily: 'Poppins', backgroundColor: 'rgb(241 245 249)'}} />
+      </div>
+      <div className='nav-menu-container'>
+        <i class="ri-notification-3-line"></i>
+        <i class="ri-chat-4-line"></i>
+        <div className='avatar'></div>
+      </div>
+    </nav>
   )
 }
 
